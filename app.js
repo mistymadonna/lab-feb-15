b4c8705893317794ae97807075b0b26de1f73747
Misty Nguyen <mistymadonna@gmail.com>
2016-02-15 09:09:04 -0800

    app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+function sum(num1, num2) {
+
+var cat = num1 + num2;
+
+
+
+//var cat = sum(4,7)
+
+
+console.log('the answer is ' + cat)
+console.log ('the sum of ' + num1  + ' and '  + num2 + ' is ' + cat)
+}
