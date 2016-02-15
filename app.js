d18ac3b0908c0eecdedf973b82c26e370bd6537f
Misty Nguyen <mistymadonna@gmail.com>
2016-02-15 09:25:32 -0800

    finished q2

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ var cat = num1 + num2;
 console.log('the answer is ' + cat)
 console.log ('the sum of ' + num1  + ' and '  + num2 + ' is ' + cat)
 }
+
+
+
+
+function multiply(num1, num2) {
+
+var cat = num1 * num2;
+
+
+
+
+console.log('the answer is ' + cat)
+console.log ('the product of ' + num1  + ' and '  + num2 + ' is ' + cat)
+}
