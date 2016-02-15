00990b9ffdd5671a2a71aab3a471ee45b936b27e
Misty Nguyen <mistymadonna@gmail.com>
2016-02-15 09:30:08 -0800

    beautified

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function sum(num1, num2) {
 
-var cat = num1 + num2;
+  var cat = num1 + num2;
 
 
 
-//var cat = sum(4,7)
+  //var cat = sum(4,7)
 
 
-console.log('the answer is ' + cat)
-console.log ('the sum of ' + num1  + ' and '  + num2 + ' is ' + cat)
+  console.log('the answer is ' + cat)
+  console.log('the sum of ' + num1 + ' and ' + num2 + ' is ' + cat)
 }
 
 
@@ -16,11 +16,11 @@ console.log ('the sum of ' + num1  + ' and '  + num2 + ' is ' + cat)
 
 function multiply(num1, num2) {
 
-var cat = num1 * num2;
+  var cat = num1 * num2;
 
 
 
 
-console.log('the answer is ' + cat)
-console.log ('the product of ' + num1  + ' and '  + num2 + ' is ' + cat)
+  console.log('the answer is ' + cat)
+  console.log('the product of ' + num1 + ' and ' + num2 + ' is ' + cat)
 }
