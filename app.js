875414144ba4f711ec938ef014c220bf74f474d5
Misty Nguyen <mistymadonna@gmail.com>
2016-02-15 20:18:17 -0800

    finished assignment and fixed errors

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ function sum(num1, num2) {
 
   var summation = num1 + num2;
 
-   //  console.log('The answer is ' + summation + '.')
+  //  console.log('The answer is ' + summation + '.')
   //  console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + summation + '.')
-  pTagOne.textContent = 'The answer is ' + summation + 'the sum of ' + num1 + ' and ' + num2 + ' is ' + summation + '.';
+  pTagOne.textContent = 'The sum of ' + num1 + ' and ' + num2 + ' is ' + summation + '.';
   return summation;
 }
 
@@ -19,7 +19,7 @@ function multiply(num1, num2) {
   //console.log('The answer is ' + multiplies + '.')
   //console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + multiplies + '.')
 
-  pTagTwo.textContent = 'The answer is ' + multiplies + ' the product of ' + num1 + ' and ' + num2 + ' is ' + multiplies + '.';
+  pTagTwo.textContent = 'The product of ' + num1 + ' and ' + num2 + ' is ' + multiplies + '.';
   return multiplies;
 }
 
@@ -31,7 +31,7 @@ function sumAndMultiply(num1, num2, num3) {
     //  console.log(num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + prob3sum + '.');
     //  console.log('The numbers ' + num1 + ' and ' + num2 + ' and ' + num3 + ' have a product of ' + prob3multiply) + '.';
 
-  pTagThree.textContent = num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + prob3sum + totalSum + '. The numbers ' + num1 + ' and ' + num2 + ' and ' + num3 + ' have a product of ' + prob3multiply + '.'
+  pTagThree.textContent = num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + prob3sum + '. The numbers ' + num1 + ' and ' + num2 + ' and ' + num3 + ' have a product of ' + prob3multiply + '.'
   return [prob3sum, prob3multiply];
 }
 
